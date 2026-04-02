b9b3d7725ede96e011d00a5df51027a803274291
carloperino01-maker <carloperino01@gmail.com>
2026-04-01 21:39:20 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1687,5 +1687,5 @@ const server = http.createServer(async (req, res) => {
 });
 
 server.listen(port, "0.0.0.0", () => {
-  console.log(\`Camasa Signature Renderer running on port \${port}\`);
+  console.log(`Camasa Signature Renderer running on port ${port}`);
 });
